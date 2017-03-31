6aab209c9619efa51e6dcd883c5000bbfe93a028
kongaik <kongaik.lee@gmail.com>
2017-03-31 15:55:59 +0800

    Add new function to main.c

--- main.c
+++ main.c
@@ -4,8 +4,14 @@
 void hello(void)
 {
 	print(stdout,"This literak is displayed on standard output\n");
-	print(stdout,"Make further changes in feature1 branch")
+	print(stdout,"Make further changes in feature1 branch");
 }
+
+void helloMy(void)
+{
+	print(stdout,"This a new function added to feature1 branch\n");
+}
+
 
 int main(void)
 {
